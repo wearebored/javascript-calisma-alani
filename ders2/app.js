81eebcb3022eab6fda25d7d6d323a6d9e1ebd209
enes <enesooo78@gmail.com>
2022-08-11 02:14:10 +0300

    ert

---
+++ ders2/app.js
@@ -0,0 +1,32 @@
+console.log("merhaba")
+
+document.write("selam")
+//alert("rr")
+const num1 = 34;
+console.log(num1)
+var a = 2;
+let b = 3;
+const c = 4;
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log("var işlemi")
+console.log(a)
+{
+    var a = 10;
+    console.log(a);
+}
+console.log(a)
+console.log("let işlemi değişken değiştirme")
+console.log(b)
+{
+    b = 12;
+    console.log(b);
+}
+console.log(b)
+
+{
+
+
+}
