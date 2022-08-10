d20c4393b70526c99c873d0d42218c23fe37226d
enes <enesooo78@gmail.com>
2022-08-10 18:19:07 +0300

    tamma

---
+++ app1.js
@@ -0,0 +1,48 @@
+console.log("app1.js is running");
+//alert("hello")
+document.write("merhaba");
+//let firstname = prompt("enter your name:")
+//alert(firstname +" "+ firstname+" ")
+//let  sonuç = confirm("are you legal?")
+//console.log(sonuç)
+var a = 10;
+{
+
+    let b = 7;
+    console.log("b=" + b)
+}
+console.log("a=" + a);
+  
+document.write(a);
+var num1 = 22;
+console.log(num1);
+
+var num1 = 33;
+console.log(num1);
+
+
+let num2 = 44;
+console.log(num2)
+
+let num3 = 55;
+console.log(num3)
+
+const name1 = {
+    name : "enes",
+    age : 25,
+    şehir : "balıkesir",
+    ülke : "türkiye",
+
+}
+console.log(name1)
+
+const name2 = name1
+
+console.log(name2)
+
+name2.name = "göktaş";
+
+console.log(name2)
+console.log(name1)
+
+ 
