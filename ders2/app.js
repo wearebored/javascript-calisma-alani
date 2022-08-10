037074d40e4de96df7273830a3be41d313cb8338
enes <enesooo78@gmail.com>
2022-08-11 02:57:41 +0300

    tr

--- ders2/app.js
+++ ders2/app.js
@@ -1,32 +1,46 @@
-console.log("merhaba")
+// console.log("merhaba")
 
-document.write("selam")
-//alert("rr")
-const num1 = 34;
-console.log(num1)
-var a = 2;
-let b = 3;
-const c = 4;
-console.log(a);
-console.log(b);
-console.log(c);
+// document.write("selam")
+// //alert("rr")
+// const num1 = 34;
+// console.log(num1)
+// var a = 2;
+// let b = 3;
+// const c = 4;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-console.log("var işlemi")
+// console.log("var işlemi")
+// console.log(a)
+// {
+//     var a = 10;
+//     console.log(a);
+// }
+// console.log(a)
+// console.log("let işlemi değişken değiştirme")
+// console.log(b)
+// {
+//     b = 12;
+//     console.log(b);
+// }
+// console.log(b);
+
+// console.log("1. sayının tipi:", typeof a)
+
+// let deger1 = +prompt("bir veri giriniz:");
+// console.log(typeof deger1);
+
+const a = 5;
 console.log(a)
-{
-    var a = 10;
-    console.log(a);
-}
+// a = 7
 console.log(a)
-console.log("let işlemi değişken değiştirme")
-console.log(b)
-{
-    b = 12;
-    console.log(b);
+
+const car = {
+    color : "red",
+    model : "s serisi",
+    yakıt : "dizel",
 }
-console.log(b);
-
-console.log("1. sayının tipi:", typeof a)
-
-let deger1 = +prompt("bir veri giriniz:");
-console.log(typeof deger1);
+console.log(car);
+car.model = "y serisi";
+console.log(car);
