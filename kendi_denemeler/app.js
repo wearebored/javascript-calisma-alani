05998543639db6c32e2a5f9ddb01b9d14eefce00
enes <enesooo78@gmail.com>
2022-08-11 17:04:53 +0300

    deneme1

---
+++ kendi_denemeler/app.js
@@ -0,0 +1,11 @@
+console.log(i)
+for (var i = 0; i < 3; i++){
+    
+    setTimeout(() => console.log(i), 1);
+    
+}
+for (let i = 0; i < 3; i++){
+    
+    setTimeout(() => console.log(i), 1);
+
+}
