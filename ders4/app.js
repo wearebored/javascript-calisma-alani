773c91eadcd8145a889ffdce71f633ad67672b02
enes <enesooo78@gmail.com>
2022-08-16 23:58:53 +0300

    AS

---
+++ ders4/app.js
@@ -0,0 +1,29 @@
+// let a = prompt("1.değeri gir:")
+// let b = prompt("2. değeri gir:")
+// if (a>b){
+//     console.log("a, b den büyük")
+// }
+// else if (a==b) {
+//     console.log("a, b ye eşit")
+    
+// }else 
+// {
+//     console.log("a, b den küçük")
+// }
+
+// TEK ÇİFT SAYI BELİRLEME
+
+
+// let a = +prompt("bir sayı giriniz:")
+// if (a==0) {
+//     console.log(`${a} tek veya çift değildir.`)
+    
+//     }
+// else if (a%2==1) {
+//     console.log(`${a} tek sayıdır.`)
+// }
+// else{
+//     console.log(`${a} çift sayıdır.`)
+
+// }
+
