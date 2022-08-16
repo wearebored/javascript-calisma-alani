14ac7f58f53e79d15e725675aed4b97efb5c73b5
enes <enesooo78@gmail.com>
2022-08-16 15:28:22 +0300

    as

--- ders3/app.js
+++ ders3/app.js
@@ -1,6 +1,9 @@
+// let sayı1 = prompt("Birinci sayıyı giriniz:")
+// let sayı2 = prompt("2.sayıyı giriniz:")
+// console.log(`2 sayının toplamı:${+sayı1 + +sayı2}`)
+// console.log(`2 sayının çarpımı:${sayı1 * sayı2}`)
 
-let a = {
-    kullanıcı : prompt("°kullanıcı gir"),
-    şifre : prompt("şifre gir")
-}
-console.log(`°F değeriniz: ${a*(9/5)+32}`)
+
+
+
+
