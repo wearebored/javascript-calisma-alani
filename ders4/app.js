45d1ab75176f52ad8a11cc0bd3cd64045f734098
enes <enesooo78@gmail.com>
2022-08-17 03:12:58 +0300

    asd

--- ders4/app.js
+++ ders4/app.js
@@ -42,5 +42,50 @@
 //     console.log("girdiğiniz sayı negatif sayıdır.")
 // }
 
+// 3 SAYI ARASINDAN EN BÜYÜĞÜNÜ BULMA
+
+// const a = prompt("1. sayıyı giriniz:")
+// const b = prompt("2. sayıyı giriniz:")
+// const c = prompt("3. sayıyı giriniz:")
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// if (a>b&&a>c){
+//     console.log(`${a} girilen en büyük sayıdır.`)
+// }
+// else if (b>a && b>c){
+//     console.log(`${b} girilen en büyük sayıdır.`)
+// }
+// else{
+//     console.log(`${c} girilen en büyük sayıdır.`)
+// }
+
+
+// HESAP MAKİNASI 
+// let a = +prompt("1. sayıyı giriniz:")
+// let b = +prompt("2. sayıyı giriniz:")
+// let işlem = prompt("yapılacak işlemi yazınız:")
+
+// if (işlem== "*"){
+//     console.log(`${a}*${b}=${a*b}`)
+// }
+// if (işlem=="/"){
+//     console.log(`${a}/${b}=${a/b}`)
+// }
+// if (işlem=="-"){
+//     console.log(`${a}-${b}=${a-b}`)
+// }
+// if (işlem=="+"){
+//     console.log(`${a}+${b}=${a+b}`)
+
+
+// İLK VE SON HARF SİLME
+
+// let a = prompt("bir cümle yazınız.")
+// console.log(`yazdığınız cümle:${a}`)
+// a = a.substring(1, (a.length-1))
+// console.log(`ilk ve son harf çıkarılmış hali:${a}`)
+
+
 
 
