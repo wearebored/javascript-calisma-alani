7b2019c1b30e02f77eeced8205f2cd9067588da1
enes <enesooo78@gmail.com>
2022-08-19 01:56:38 +0300

    döngüler

--- ders5/app.js
+++ ders5/app.js
@@ -1,8 +1,45 @@
 //  N KADAR SAYI TOPLAMA
 
-let adet = prompt("kaç adet sayı toplayacaksınız:")
-let toplam = 0
-for(i=1; i<=adet; i++){
-    toplam += +prompt(`${i}. sayıyı giriniz:`)
-}
-console.log(`Girdiğin ${adet} adet sayının toplamları: ${toplam}`)
+// let adet = prompt("kaç adet sayı toplayacaksınız:")
+// let toplam = 0
+// for(i=1; i<=adet; i++){
+//     toplam += +prompt(`${i}. sayıyı giriniz:`)
+// }
+// console.log(`Girdiğin ${adet} adet sayının toplamları: ${toplam}`)
+
+
+// RASTGELE SAYI ÜRETME
+// for (i = 0; i<10; i++){
+//     console.log(Math.floor(Math.random()*100));
+// }
+
+// ASAL SAYI BULMA
+// let sayı = +prompt("Asallığı kontrol etmek için bir sayı giriniz:")
+// let sayaç = 0
+// for(i=2; i<=sayı; i++){
+//     if(sayı%i===0){
+//         sayaç+=1
+
+//     }
+
+// }
+// if(sayaç>1){
+//     console.log(`${sayı} asal sayı değildir.`);
+// }
+// else{
+//     console.log(`${sayı} asal sayıdır.`);
+// }
+
+// WHİLE DÖNGÜ
+// let sayı = +prompt("Bir sayı giriniz:")
+// while (true) {
+//     if(sayı>=0 && sayı<=100){
+//         console.log("girilen değer sayı");
+//         break
+//     }
+//     else{
+//         sayı = +prompt("tekrar sayı giriniz:")
+//     }
+    
+// }
+
