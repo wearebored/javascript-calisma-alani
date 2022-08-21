28d1f0c4db7858ee9967482ba9c770b5316fa2b2
enes <enesooo78@gmail.com>
2022-08-21 16:04:25 +0300

    girilen en büyük sayı

--- ders6/app.js
+++ ders6/app.js
@@ -24,3 +24,18 @@
 // };
 
 // yaş_hesap("enes", 1997);
+
+// 3 SAYININ EN BÜYÜĞÜNÜ BULAN FONKSİYON
+
+// const en_büyük = function (n1, n2, n3 = Number.NEGATIVE_INFINITY) {
+//   if (n1 >= n2 && n1 >= n3) {
+//     console.log(`Girilen en büyük sayı : ${n1}`);
+//   } else if (n2 >= n1 && n2 >= n3) {
+//     console.log(`Girilen en büyük sayı : ${n2}`);
+//   } else if (n3 >= n1 && n3 >= n2) {
+//     console.log(`Girilen en büyük sayı : ${n3}`);
+//   } else {
+//     console.log("girilen sayılarda hata var.");
+//   }
+// };
+// en_büyük(-24, -12, 2);
