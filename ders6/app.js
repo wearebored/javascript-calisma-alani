136b72704d5faa21d1f605506b4bf305d1fcaa3b
enes <enesooo78@gmail.com>
2022-08-21 05:40:44 +0300

    yaş hesabı

---
+++ ders6/app.js
@@ -0,0 +1,17 @@
+// console.log("start");
+
+// FONKSİYONLARR
+
+// function yaş_yazdır(yaş) {
+//   console.log(`Ben ${yaş} yaşındayım.`);
+// }
+// yaş_yazdır(24);
+
+// YAŞ HESABI
+
+// function yaş_hesabı(isim, doğum_yılı) {
+//   let yaşı = 2022 - doğum_yılı;
+//   console.log(`${isim} yaşınız: ${yaşı}`);
+//   return isim, yaşı;
+// }
+// yaş_hesabı("enes", 1997);
