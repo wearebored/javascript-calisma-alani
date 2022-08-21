60aa9e18c5f3021162e2b0c6ca04771718b0a198
enes <enesooo78@gmail.com>
2022-08-21 15:53:35 +0300

    2. yöntem

--- ders6/app.js
+++ ders6/app.js
@@ -15,3 +15,12 @@
 //   return isim, yaşı;
 // }
 // yaş_hesabı("enes", 1997);
+
+//         2. YÖNTEMLE FONKS ATAMA MODERN JAVASCRİPT
+
+// const yaş_hesap = function (isim, doğum_yılı) {
+//   let yaşı = 2022 - doğum_yılı;
+//   console.log(`${isim} yaşınız: ${yaşı}`);
+// };
+
+// yaş_hesap("enes", 1997);
