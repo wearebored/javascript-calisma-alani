3efcdea105b9b7ce74f0497ba25062082072b98e
enes <enesooo78@gmail.com>
2022-08-23 23:41:10 +0300

    örnek2

--- ders7/app.js
+++ ders7/app.js
@@ -2,3 +2,8 @@
 
 // const yaş_hes = (a) => 2022 - a;
 // console.log(yaş_hes(1997));
+
+// ARROW FUNCTİON DAİRE ALAN HESABI
+
+// const daire_a = (yarıçap) => Math.PI * yarıçap ** 2;
+// console.log(daire_a(2));
