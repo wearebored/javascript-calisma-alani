fd7cac8439dc24bcf7b247bfab48a8f1f1e0008b
enes <enesooo78@gmail.com>
2022-08-24 00:20:20 +0300

    fib

--- ders7/app.js
+++ ders7/app.js
@@ -6,4 +6,17 @@
 // ARROW FUNCTİON DAİRE ALAN HESABI
 
 // const daire_a = (yarıçap) => Math.PI * yarıçap ** 2;
-// console.log(daire_a(2));
+// console.log(daire_a(+prompt("Dairenin yarçapını giriniz:")));
+
+// FİBONACCI ÇÖZÜM
+
+const fib = function (n) {
+  let başlangıç = 1;
+  let son_2 = 1;
+  for (let sayaç = 0; sayaç < n - 3; sayaç++) {
+    son_2 = başlangıç;
+    başlangıç += son_2;
+  }
+  return başlangıç;
+};
+console.log(fib(6));
