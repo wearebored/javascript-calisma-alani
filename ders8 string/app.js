c9dc0a3c36f84883edadae88c6fd653b46a9b6fb
enes <enesooo78@gmail.com>
2022-08-25 19:28:46 +0300

    kelime eşitlik sorgu

--- ders8 string/app.js
+++ ders8 string/app.js
@@ -2,7 +2,20 @@
 
 // STRİNGLER
 
-let names = "enes";
-let lastname = "GOKTAS";
-console.log(names.toUpperCase());
-console.log(lastname.toLowerCase());
+// let names = "enes";
+// let lastname = "GOKTAS";
+// console.log(names.toUpperCase());
+// console.log(lastname.toLowerCase());
+
+// KELİME EŞİTLİK SORGUSU
+
+// let ifade1 = prompt("1. ifadeyi yazınız:");
+// let ifade2 = prompt("2. ifadeyi yazınız:");
+// let eşitmi = (ifade1, ifade2) => {
+//   let sonuc =
+//     ifade1.toLocaleLowerCase === ifade2.toLocaleLowerCase
+//       ? `${ifade1} ile ${ifade2} eşittir.`
+//       : `${ifade1} ile ${ifade2} eşit değildir.`;
+//   return sonuc;
+// };
+// console.log(eşitmi(ifade1, ifade2));
